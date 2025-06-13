44d0557f665941c582fa12ef20ebe1f492dd3a8f
Amir Mohammad <amir.mhsanji1.1380@gmail.com>
2025-06-13 23:40:28 +0330

    update user response api internal

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,4 +1,5 @@
-import { ApiResponse } from "@/app/(applications)/(http)/http.model";
+import { ApiStatus } from "@/app/(applications)/(http)/http.constant";
+import { ApiError, ApiResponse } from "@/app/(applications)/(http)/http.model";
 import { User } from "@/app/users/(core)/model/users.model";
 import { NextRequest, NextResponse } from "next/server";
 
@@ -9,23 +10,34 @@ export async function GET(req: NextRequest) {
   const search = req.nextUrl.search;
   const url = new URL(`/api/${search}`, process.env.NEXT_PUBLIC_API_URL);
   const response = await fetch(url.toString());
-  if (response.status) {
-    const res = (await response.json()) as ApiResponse<User>;
-    return NextResponse.json({
-      ...res,
-      results: res.results.map((item) => ({
-        ...item,
-        login: {
-          uuid: item.login.uuid,
-          username: item.login.username,
-        },
-        picture: Object.fromEntries(
-          Object.entries(item.picture).map(([key, value]) => [
-            key,
-            removeBaseUrlFromImages(value),
-          ])
+  switch (response.status) {
+    case ApiStatus.SUCCESS: {
+      const res = (await response.json()) as ApiResponse<User>;
+      return NextResponse.json({
+        ...res,
+        results: res.results.map(
+          ({ login: { username, uuid }, picture, ...item }) => ({
+            ...item,
+            login: {
+              username,
+              uuid,
+            },
+            picture: Object.fromEntries(
+              Object.entries(picture).map(([key, value]) => [
+                key,
+                removeBaseUrlFromImages(value),
+              ])
+            ),
+          })
         ),
-      })),
-    });
+      });
+    }
+
+    default:
+      const res = (await response.json()) as ApiError;
+      return NextResponse.json(
+        { error: res.error || response.statusText },
+        { status: response.status }
+      );
   }
 }
